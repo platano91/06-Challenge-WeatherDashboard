6904e3fa0357aadcc9e30ed32f3cce93d4bedc06
Samuel <salvadomo@hotmail.com>
2023-12-18 17:44:33 -0700

    variables to js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
 const API_KEY = 'b9d7e8ce9dc6dbbedf5c0bdbc338f4f9'; // OpenWeatherMap API key
+
+const searchBtn = document.getElementById('search');
+const cityInput = document.getElementById('city');
+const weatherDataDiv = document.getElementById('weather-data');
+const forecastDiv = document.getElementById('forecast');
+
